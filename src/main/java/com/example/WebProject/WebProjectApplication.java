f53588c0c271672d4b611dc096ad9a358a79d5d7
MCJoo <hjjooace@naver.com>
2020-06-11 16:59:25 +0900

    Fix the project(Initialize Setting)

--- src/main/java/com/example/WebProject/WebProjectApplication.java
+++ src/main/java/com/example/WebProject/WebProjectApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.WebProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WebProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WebProjectApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/WebProject/WebProjectController.java
@@ -0,0 +1,16 @@
+package com.example.WebProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+
+@RestController
+public class WebProjectController {
+    @GetMapping("/main")
+    public String hello() {
+        return "안녕하세요. 현재 서버시간은 " + new Date() + "입니다. \n";
+    }
+}
+
